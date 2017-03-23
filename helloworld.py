ddf81dfb8971da09b8adcc99127b815d4e230b3a
pietrom54323 <7013302@isd535.org>
2017-03-23 12:15:39 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,6 @@ from random import randint
 
 repeat_num = randint(0, 10)
 for i in range(repeat_num):
-    print ("Hello, world!!!!!")
+    print ("Hello, Lord Python!!!!!")
+    
+    #switched the print from hello world to my name 
